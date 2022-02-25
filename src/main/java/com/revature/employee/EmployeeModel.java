03efe97848bb0088549ae6276b2da15928e172da
benjaminjbonnet <benjaminbonnet07@icloud.com>
2022-02-25 17:08:54 -0500

    project lombock added benjamin

---
+++ src/main/java/com/revature/employee/EmployeeModel.java
@@ -0,0 +1,23 @@
+package com.revature.employee;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="employee")
+public class EmployeeModel {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="employeeid")
+	private Integer userId;
+	
+	
+}
